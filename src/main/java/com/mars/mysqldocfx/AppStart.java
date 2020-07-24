8123bf551c0dfa8aa540727442a0cb53003edd53
Mars Shen <look1239@qq.com>
2020-07-24 18:34:05 +0800

    修改生成规则, 现在可以生成exe

---
+++ src/main/java/com/mars/mysqldocfx/AppStart.java
@@ -0,0 +1,14 @@
+package com.mars.mysqldocfx;
+
+/**
+ * @Program: MysqlDocFx
+ * @ClassName:AppStart
+ * @Description:
+ * @Author: shenchengjian
+ * @Create: 2020-07-24 18:02
+ **/
+public class AppStart {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
